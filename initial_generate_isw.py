1e3e609cb1e59bdcc56e86665dcd4858506acc8a
Luis Fernando Machado Poletti Valle <lmachado@student.ethz.ch>
2022-03-07 14:23:27 +0100

    Use box settings from papers, add CLASS example for L-ISW

--- initial_generate_isw.py
+++ initial_generate_isw.py
@@ -9,48 +9,48 @@ FILEPATH = "/cluster/home/lmachado/run_data/run_0/"
 data = np.load(f"{FILEPATH}/shells.npy", mmap_mode='r')
 info = np.load(f"{FILEPATH}/shell_info.npy")
 
-ShellInfo = namedtuple("ShellInfo", ["fits_filename", "id", "z_low", "z_high", "dontknow1", "dontknow2", "dontknow3"])
+ShellInfo = namedtuple("ShellInfo", ["fits_filename", "id", "z_low", "z_high", "comoving_radius_z_low", "comoving_radius_z_high", "comoving_radius_z_avg"])
 info = [ShellInfo(*row) for row in info]
 
 # Obtain redshift values for each slice
 zedge_min = np.array([row.z_low for row in info])
 zedge_max = np.array([row.z_high for row in info])
 
-# setup basic cosmology using the TheoryCL package and creating look up tables
-# for the linear growth functions
-
-omega_m             = 0.25              # matter density
-omega_l             = 1 - omega_m       # lambda density
-h0                  = 0.6736               # Hubble constant (H0/100)
-zmin_lookup         = 0.                # minimum redshift for lookup table
-zmax_lookup         = 10.               # maximum redshift for lookup table
-zbin_num            = 10000             # number of points in the lookup table
-zbin_mode           = 'log'             # either 'linear' or 'log' binnings in z (log means log of 1+z)
+# Parameters obtained from params.yml
+# Matter content: contains CDM + Baryons + Neutrinos
+omega_m             = 0.26
+omega_l             = 1 - omega_m
+h0                  = 0.6736
+zmin_lookup         = 0.
+zmax_lookup         = 10.
+zbin_num            = 10000
+zbin_mode           = "log"
 
 GISW = pyGenISW.isw.SphericalBesselISW()
 GISW.cosmo(omega_m=omega_m, omega_l=omega_l, h0=h0)
 GISW.calc_table(zmin=zmin_lookup, zmax=zmax_lookup, zbin_num=zbin_num, zbin_mode=zbin_mode)
 
-# spherical Bessel Transform (SBT) setup
-zmin                = 0.                # minimum redshift for the SBT
-zmax                = 4.                # maximum redshift for the SBT
-#zedge_min           =                   # the minimum of each redshift slice
-#zedge_max           =                   # the maximum of each redshift slice
-Lbox                = 3072.             # size of the simulation box
-kmin                = 2.*np.pi/Lbox     # minimum k
-kmax                = 0.1               # maximum k
-lmax                = None              # if you want to specify the maximum l for the SBT transform
-nmax                = None              # if you want to specify the maximum n for the SBT transform
-uselightcone        = True              # set to True if the
-boundary_conditions = 'normal'          # boundary conditions for the SBT, either 'normal' or 'derivative'
+# Setup for the SBT
+zmin                = 0.
+zmax                = 4.
+# Lbox Units: Mpc/h (according to pyGenISW paper, Section 2.1)
+Lbox                = 900
+kmin                = 2.*np.pi/Lbox
+# kmax selected to limit analysis to linear perturbations.
+# Since non-linear perturbations happen for k > 0.1 h / Mpc
+kmax                = 0.1
+lmax                = None
+nmax                = None
+uselightcone        = True
+boundary_conditions = "normal" # Either normal or derivative
 
 GISW.setup(zmin, zmax, zedge_min, zedge_max, kmin=kmin, kmax=kmax,
            lmax=lmax, nmax=nmax, uselightcone=uselightcone,
            boundary_conditions=boundary_conditions)
 
-# converts each redshift slice into spherical harmonic alms
+# Convert redshift slices into Spherical Harmonic Alm values
 for i in range(0, len(zedge_min)):
-    # map of the density for redshift slice i corresponding to zedge_min[i]
+    # Map of the density for redshift slice i
     counts = data[i]
     map_slice = counts
 
@@ -63,33 +63,38 @@ for i in range(0, len(zedge_min)):
 
     # Compute alm from map_slice,
     # and store inside temp/ directory
-    print(f" about to perform slice2alm in index i: {i}")
+    print(f"About to perform slice2alm in index i: {i}")
     GISW.slice2alm(map_slice, i)
 
 print("Done with for loop")
 
-# converts spherical harmonic alms for the slices to the SBT coefficients
+# Convert Spherical Harmonic Alm values to SBT coefficients
 GISW.alm2sbt()
 
-# you can store the SBT coefficients to avoid recomputing this again by running:
-sbt_fname_prefix = "testnamesbt"# name of prefix for SBT file
+# Optionally, store SBT coefficients
+sbt_fname_prefix = "testnamesbt" # Prefix for output file
 GISW.save_sbt(prefix=sbt_fname_prefix)
 
-# create ISW for contributions between zmin_isw and zmax_isw
+# Compute ISW
+# Optionally, pass in a range of redshifts within which to
+# compute ISW
 alm_isw = GISW.sbt2isw_alm()
 
-# convert alm to healpix map
+# Convert Alm values to healpix map
 nside = 256
 map_isw = hp.alm2map(alm_isw, nside) * GISW.Tcmb
 
 # After generating map, create power spectrum
 cl = hp.anafast(map_isw, lmax=1000)
+cl *= (1e6)**2 # anafast returns Kelvin^2, but want to plot in microKelvin^2
 ell = np.arange(len(cl))
 
 plt.figure(figsize=(10, 5))
-plt.plot(ell, ell * (ell + 1) * cl)
+plt.plot(ell, cl)
 plt.xlabel("$\ell$")
-plt.ylabel("$\ell(\ell+1)C_{\ell}$")
+plt.ylabel("$C_{\ell}$")
+plt.yscale("log")
 plt.grid()
 plt.savefig("angular_power_spectrum.png")
 plt.show()
+
